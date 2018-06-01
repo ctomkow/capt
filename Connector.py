22b9bc132553f417175a19dfa6a946de256bdec0
Craig Tomkow <ctomkow@gmail.com>
2018-06-01 12:19:29 -0600

    tweaked config file, more core code

--- Connector.py
+++ Connector.py
@@ -2,11 +2,14 @@
 # May 22, 2018
 #
 # Connector class to pull state from Cisco Prime Infrastructure
-# Note: Use API v3 in calls!!!
+# Note: Use API v3 in calls (released in Prime 3.3
+#       When Prime goes to 3.4, change to APIv4
+#       syncDevices call only exists in API v1 it looks like according to documentation (and v1 is depreciated)
 
 
 # 3rd part imports
 import requests
+import json
 
 
 class Connector:
@@ -22,20 +25,47 @@ class Connector:
         password = passwd
         cpi_ipv4_address = cpi_ipv4_addr
 
+
+    def print_info(self, dev_id):
+
+        url = f"https://{cpi_ipv4_address}/webacs/api/v3/data/Devices/{dev_id}.json"
+        req = requests.get(url, verify=False, auth=(username, password))
+        print(json.dumps(req.json(), indent=4))
+
+    def print_detailed_info(self, dev_id):
+
+        url = f"https://{cpi_ipv4_address}/webacs/api/v3/data/InventoryDetails/{dev_id}.json"
+        req = requests.get(url, verify=False, auth=(username, password))
+        print(json.dumps(req.json(), indent=4))
+
+    # Forcing a sync is broken
+    def sync(self, dev_ipv4_address):
+
+        url = f"https://{cpi_ipv4_address}/webacs/api/v3/op/devices/syncDevices.json"
+        payload = { "syncDevicesDTO" : { "devices" : { "device" : [ { "address" : f"{dev_ipv4_address}" } ] } } }
+        req = requests.post(url, verify=False, auth=(username, password), json=payload)
+
+    # device id is needed for most future API calls
     def get_dev_id(self, dev_ipv4_address):
 
         url = f"https://{cpi_ipv4_address}/webacs/api/v3/data/Devices.json?ipAddress=\"{dev_ipv4_address}\""
         req = requests.get(url, verify=False, auth=(username, password))
         return req.json()['queryResponse']['entityId'][0]['$']
 
-    def is_reachable(self):
+    def get_reachability(self, dev_id):
 
-        pass
+        url = f"https://{cpi_ipv4_address}/webacs/api/v3/data/Devices/{dev_id}.json"
+        req = requests.get(url, verify=False, auth=(username, password))
+        return req.json()['queryResponse']['entity'][0]['devicesDTO']['reachability']
 
-    def get_stack_members(self):
+    # Switch chassis info
+    def get_stack_members(self, dev_id):
 
-        pass
+        url = f"https://{cpi_ipv4_address}/webacs/api/v3/data/InventoryDetails/{dev_id}.json"
+        req = requests.get(url, verify=False, auth=(username, password))
+        return req.json()['queryResponse']['entity'][0]['inventoryDetailsDTO']['chassis']
 
+    # CDP neighbour info gets populated when the device syncs
     def get_cdp_neighbours(self, dev_id):
 
         url = f"https://{cpi_ipv4_address}/webacs/api/v3/data/InventoryDetails/{dev_id}.json"
--- Verify.py
+++ Verify.py
@@ -10,18 +10,17 @@ import _thread
 
 # local imports
 import Config
-
 import Connector
 from Connector import Connector
+import json
 
 class Verify:
 
 
     def __init__(self):
 
-        # argument parsing
+        # argument parsing will go here
 
-        #config loading
         Config.load_configuration()
 
         self.main()
@@ -31,18 +30,37 @@ class Verify:
 
         ### within a thread... ###
 
+        # get reachability status
+        #api_call = Connector(Config.username, Config.password, Config.cpi_ipv4_address)
+        #dev_reachability = api_call.get_reachability(api_call.get_dev_id(Config.device()))
+        #print(json.dumps(dev_reachability, indent=4))
+
         # CDP neighbour call
-        api_call = Connector(Config.username, Config.password, Config.cpi_ipv4_address)
-        dev_cdp_neighbours = api_call.get_cdp_neighbours(api_call.get_dev_id(Config.device()))
+        #api_call = Connector(Config.username, Config.password, Config.cpi_ipv4_address)
+        #dev_cdp_neighbours = api_call.get_cdp_neighbours(api_call.get_dev_id(Config.device()))
+        #dev_cdp_neighbours = dev_cdp_neighbours['cdpNeighbor']
+        #print(json.dumps(dev_cdp_neighbours, indent=4))
+        #data = next((item for item in dev_cdp_neighbours if item["neighborDeviceName"] == "SEPC0626BD2690F"))
+        #print(data)
 
-        print(dev_cdp_neighbours)
+        # force sync device,need NBI_WRITE access
+        #api_call = Connector(Config.username, Config.password, Config.cpi_ipv4_address)
+        #api_call.sync(Config.device())
 
-        # grab state for device
+        # get switch stack info
+        #api_call = Connector(Config.username, Config.password, Config.cpi_ipv4_address)
+        #dev_stack_info = api_call.get_stack_members(api_call.get_dev_id(Config.device()))
+        #print(json.dumps(dev_stack_info, indent=4))
+
+        # print basic switch information
+        #api_call = Connector(Config.username, Config.password, Config.cpi_ipv4_address)
+        #api_call.print_info(api_call.get_dev_id(Config.device()))
 
 
-        # parse and store only the state specified from Connector module
-        # compare old and new state files
-        # yadda yadda yadda
+        # print detailed switch information
+        #api_call = Connector(Config.username, Config.password, Config.cpi_ipv4_address)
+        #api_call.print_detailed_info(api_call.get_dev_id(Config.device()))
+
 
 
 if __name__ == '__main__':
