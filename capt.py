cadba40ffbbed6218dce19999cca049e72941655
Craig Tomkow <ctomkow@gmail.com>
2018-06-19 12:21:23 -0600

    replacing print statements with logging

--- capt.py
+++ capt.py
@@ -136,7 +136,7 @@ class capt:
                         t = threading.Thread(target=self.test_api_calls, args=(switch_ipv4_address_list[0], config.username,
                                                                 config.password, config.cpi_ipv4_address, logger))
                 except KeyError:
-                    sys_logger.critical("Thread failed to execute function. Likely no procedure selected as 'yes' in configuration.")
+                    sys_logger.critical("Thread failed to execute function.")
                     sys.exit(1)
 
                 threads.append(t)
@@ -152,11 +152,11 @@ class capt:
 
     def upgrade_code(self, switch_ipv4_address, cpi_username, cpi_password, cpi_ipv4_address, logger):
 
-        print("{}: UPGRADE_CODE".format(switch_ipv4_address))
+        logger.info("Upgrading code.")
 
-        ############################
-        ###### PRE_PROCESSING ######
-        ############################
+        #--------------------------#
+        #      PRE_PROCESSING      #
+        #--------------------------#
         api_call = connector(cpi_username, cpi_password, cpi_ipv4_address)
 
         sw = switch()
@@ -164,59 +164,56 @@ class capt:
 
         sw.id = api_call.get_dev_id(sw.ipv4_address)
 
-        ####################
-        ###### BEFORE ######
-        ####################
+        #--------------------------#
+        #   PRE_STATE_COLLECTION   #
+        #--------------------------#
 
-        ###### 1. check for reachability
-
-        print("{}: TESTING REACHABILITY".format(sw.ipv4_address))
-
+        # 1. check for reachability
+        logger.info("Testing reachability ...")
         timeout = time.time() + 60*5 # 5 minute timeout starting now (this is before the code upgrade, so short timeout)
+        logger.info("Timeout set to {} minutes.".format(timeout))
+
         while not self.reachable(sw, api_call):
-            print('.', end='', flush=True)
             time.sleep(5)
+            logger.debug("Timeout counter: {}".format(timeout))
+            logger.debug("Switch reachability state: {}".format(sw.reachability))
             if time.time() > timeout:
-                print("")
-                print("{}: ERROR - 5 minutes and switch hasn't responded. Exiting script.".format(sw.ipv4_address))
+                logger.error("Timed out. Not reachable.")
                 sys.exit(1)
 
-        print("")
-        print("{}: {}".format(sw.ipv4_address, sw.reachability))
+        logger.debug("Switch reachability state: {}".format(sw.reachability))
+        logger.info("Reachable!")
 
-
-        ##### 2. force sync of switch state
-
-        print("{}: SYNCHRONIZE DEVICE".format(sw.ipv4_address))
+        # 2. force sync of switch state
+        logger.info("Synchronizing ...")
         old_sync_time = api_call.get_sync_time(sw.id)
         api_call.sync(sw.ipv4_address) # force a sync!
         time.sleep(5) # don't test for sync status too soon (CPI delay and all that)
-
         timeout = time.time() + 60 * 10  # 10 minute timeout starting now
-        while not self.synchronized(sw, api_call):
-            print('.', end='', flush=True)
-            time.sleep(5)
-            if time.time() > timeout:
-                print("")
-                print("{}: ERROR - 10 minutes and switch hasn't synced. Exiting script.".format(sw.ipv4_address))
-                sys.exit(1)
+        logger.info("Timeout set to {} minutes.".format(timeout))
 
-        print("")
-        print("{}: {}".format(sw.ipv4_address, sw.sync_state))
+        while not self.synchronized(sw, api_call):
+            time.sleep(5)
+            logger.debug("Timeout counter: {}".format(timeout))
+            logger.debug("Switch sync state: {}".format(sw.sync_state))
+            if time.time() > timeout:
+                logger.error("Timed out. Sync failed.")
+                sys.exit(1)
 
         new_sync_time = api_call.get_sync_time(sw.id)
         if old_sync_time == new_sync_time: # KEEP CODE! needed for corner case issue where force sync fails (e.g. code 03.03.03)
-            print("{}: ERROR - sync failed. Cancelling script. Proceed with upgrade manually ***".format(sw.ipv4_address))
+            logger.error("Before and after sync time is the same. Sync failed.")
             sys.exit(1)
 
-        ###### 3. get current software version
+        logger.debug("switch sync state: {}".format(sw.sync_state))
+        logger.info("Synchronized!")
 
+        # 3. get current software version
         sw.pre_software_version = api_call.get_software_version(sw.id)
-        print("{}: INFO - current version is {}".format(sw.ipv4_address, sw.pre_software_version))
+        logger.info("Software version: {}".format(sw.pre_software_version))
 
-
-        ###### 4. get stack members
-
+        # 4. get stack members
+        logger.info("Getting stack members ...")
         sw.pre_stack_member = api_call.get_stack_members(sw.id)
         sw.pre_stack_member = sorted(sw.pre_stack_member, key=lambda k: k['name'])  # sort the list of dicts
 
@@ -226,44 +223,49 @@ class capt:
 
         sw.pre_stack_member_name = [x['name'] for x in sw.pre_stack_member]  # extract name values
         sw.pre_stack_member_desc = [x['description'] for x in sw.pre_stack_member]  # extract description values
-        print("{}: STACK MEMBERS - {}".format(sw.ipv4_address, sw.pre_stack_member_name))
-        print("{}: STACK MEMBERS - {}".format(sw.ipv4_address, sw.pre_stack_member_desc))
 
-        ###### 5. get VLANs
-        ### TO-DO
-        ##
+        logger.debug("Stack member names: {}".format(sw.pre_stack_member_name))
+        logger.debug("Stack member descriptions: {}".format(sw.pre_stack_member_desc))
+        logger.info("Stack members stored!")
+
+        # 5. get VLANs
+        # TO-DO
+        #
         #
 
-        ###### 6. get CDP neighbour state
-
+        # 6. get CDP neighbour state
+        logger.info("Getting CDP neighbours ...")
         sw.pre_cdp_neighbour = api_call.get_cdp_neighbours(sw.id)
         sw.pre_cdp_neighbour = sorted(sw.pre_cdp_neighbour, key=lambda k: k['nearEndInterface']) # sort the list of dicts
 
         # Using 'nearEndInterface' key. The 'phyInterface' number changes between code upgrade versions
 
         sw.pre_cdp_neighbour_nearend = [x['nearEndInterface'] for x in sw.pre_cdp_neighbour] # extract nearEnd values
-        print("{}: CDP NEIGHOURS - {}".format(sw.ipv4_address, sw.pre_cdp_neighbour_nearend))
 
-        print("")
-        print("{}: PRE-PROCESSING COMPLETE".format(sw.ipv4_address))
+        logger.debug("CDP neighbours: {}".format(sw.pre_cdp_neighbour))
+        logger.debug("CDP neighbours near-end: {}".format(sw.pre_cdp_neighbour_nearend))
+        logger.info("CDP neighbours stored!")
 
-        ####################
-        ###### RELOAD ######
-        ####################
+        logger.info("State collection complete!")
 
-        print("{}: REBOOTING".format(sw.ipv4_address))
+        #--------------------------#
+        #          RELOAD          #
+        #--------------------------#
+
+        logger.info("Reloading ...")
         job_id = api_call.reload_switch(sw.id, "1")
+        logger.debug("Reload job_id: {}".format(job_id))
 
         timeout = time.time() + 60 * 5  # 5 minute timeout starting now
+        logger.info("Timeout set to {} minutes.".format(timeout))
         while not api_call.job_complete(job_id): # while not completed ... wait...
-            print('.', end='', flush=True)
             time.sleep(5)
+            logger.debug("Timeout counter: {}".format(timeout))
             if time.time() > timeout:
-                print("")
-                print("{}: CRITICAL - 5 minutes and CPI hasn't finished job. Exiting script.".format(sw.ipv4_address))
+                logger.error("Timed out. Sync failed.")
                 sys.exit(1)
 
-        print("\n")
+        logger.debug("Reload job {} finished.".format(job_id))
 
         # DON'T CHECK IF JOB WAS SUCCESSFUL, IT FAILS CAUSE SWITCH DROPS CONNECTIVITY WHEN REBOOTING
         # if api_call.job_successful(job_id):
@@ -272,64 +274,64 @@ class capt:
         #     print("reload job failed! exiting!")
         #     sys.exit(1)
 
-        ###################
-        ###### AFTER ######
-        ###################
+        #--------------------------#
+        #   POST_STATE_COLLECTION  #
+        #--------------------------#
 
-        ###### 1. check for reachability
-
-        print("{}: TESTING REACHABILITY".format(sw.ipv4_address))
-
+        # 1. check for reachability
+        logger.info("Testing reachability ...")
         timeout = time.time() + 60*45 # 45 minute timeout starting now
+        logger.info("Timeout set to {} minutes.".format(timeout))
         while not self.reachable(sw, api_call):
-            print('.', end='', flush=True)
             time.sleep(5)
+            logger.debug("Timeout counter: {}".format(timeout))
+            logger.debug("Switch reachability state: {}".format(sw.reachability))
             if time.time() > timeout:
-                print("")
-                print("{}: CRITICAL - 45 minutes and switch hasn't responded. Exiting script.".format(sw.ipv4_address))
-                sys.exit(1)
+            logger.error("Timed out. Not reachable.")
+            sys.exit(1)
 
-        print("\n")
-        print("{}: {}".format(sw.ipv4_address, sw.reachability))
+        logger.debug("Switch reachability state: {}".format(sw.reachability))
+        logger.info("Reachable!")
 
-        ##### 2. force sync of switch state
-
-        print("{}: SYNCHRONIZE DEVICE".format(sw.ipv4_address))
+        # 2. force sync of switch state
+        logger.info("Synchronizing ...")
         old_sync_time = api_call.get_sync_time(sw.id)
         api_call.sync(sw.ipv4_address)  # force a sync!
         time.sleep(5)  # don't test for sync status too soon (CPI delay and all that)
-
         timeout = time.time() + 60 * 10  # 10 minute timeout starting now
+        logger.info("Timeout set to {} minutes.".format(timeout))
+
         while not self.synchronized(sw, api_call):
-            print('.', end='', flush=True)
             time.sleep(5)
+            logger.debug("Timeout counter: {}".format(timeout))
+            logger.debug("Switch sync state: {}".format(sw.sync_state))
             if time.time() > timeout:
-                print("")
-                print("{}: ERROR - 10 minutes and switch hasn't synced. Exiting script.".format(sw.ipv4_address))
+                logger.error("Timed out. Sync failed.")
                 sys.exit(1)
 
-        print("")
-        print("{}: {}".format(sw.ipv4_address, sw.sync_state))
-
         new_sync_time = api_call.get_sync_time(sw.id)
-        if old_sync_time == new_sync_time:  # needed for corner case issue where force sync fails (e.g. code 03.03.03)
-            print("{}: *** ERROR - sync failed. Cancelling script. Proceed with upgrade manually ***".format(
-                sw.ipv4_address))
+        if old_sync_time == new_sync_time:  # KEEP CODE! needed for corner case issue where force sync fails (e.g. code 03.03.03)
+            logger.error("Before and after sync time is the same. Sync failed.")
             sys.exit(1)
 
-        ###### 3. get software version
+        logger.debug("switch sync state: {}".format(sw.sync_state))
+        logger.info("Synchronized!")
 
+        # 3. get software version
         sw.post_software_version = api_call.get_software_version(sw.id)
-        print("{}: INFO - new version is {}".format(sw.ipv4_address, sw.pre_software_version))
+        logger.info("Software version: {}".format(sw.post_software_version))
 
         # compare
+        logger.info("Comparing software version states ...")
         if sw.pre_software_version == sw.post_software_version:
-            print("{}: ERROR - software version before and after is the same, {}".format(sw.ipv4_address, sw.post_software_version))
+            logger.debug("Pre-software: {}".format(sw.pre_software_version))
+            logger.debug("Post-software: {}".format(sw.post_software_version))
+            logger.warning("Upgrade failed. Software is same as before.")
         else:
-            print("{}: INFO - old:{} new:{}".format(sw.ipv4_address, sw.pre_software_version, sw.post_software_version))
+            logger.info("Software is different (whew).")
 
-        ###### 4. get stack members
-
+        # 4. get stack members
+        logger.info("Getting stack members ...")
         sw.post_stack_member = api_call.get_stack_members(sw.id)
         sw.post_stack_member = sorted(sw.post_stack_member, key=lambda k: k['name'])  # sort the list of dicts
 
@@ -339,58 +341,68 @@ class capt:
 
         sw.post_stack_member_name = [x['name'] for x in sw.post_stack_member]  # extract name values
         sw.post_stack_member_desc = [x['description'] for x in sw.post_stack_member]  # extract description values
-        print("{}: STACK MEMBERS - {}".format(sw.ipv4_address, sw.post_stack_member_name))
-        print("{}: STACK MEMBERS - {}".format(sw.ipv4_address, sw.post_stack_member_desc))
+
+        logger.debug("Stack member names: {}".format(sw.post_stack_member_name))
+        logger.debug("Stack member descriptions: {}".format(sw.post_stack_member_desc))
+        logger.info("Stack members stored!")
 
         # compare states
+        logger.info("Comparing stack member states ...")
         pre_name_diff, post_name_diff = self.compare_list(sw.pre_stack_member_name, sw.post_stack_member_name)
         pre_desc_diff, post_desc_diff = self.compare_list(sw.pre_stack_member_desc, sw.post_stack_member_desc)
 
         if not pre_name_diff and not post_name_diff and not pre_desc_diff and not post_desc_diff:
-            print("INFO: {} stack members are the same before as after".format(sw.ipv4_address))
+            logger.info("Stack members are the same pre/post (noice).")
         else:
             # if the name difference exists before but not after ... switch is missing!
             if pre_name_diff:
-                print("Switch(es) no longer exists in stack! {}".format(pre_name_diff))
+                logger.error("Stack member(s) no longer part of stack!")
+                logger.error(pre_name_diff)
             # if the name difference exists after but not before ... switch was found???
             if post_name_diff:
-                print("New switch(es) detected AFTER code upgrade! {}".format(post_name_diff))
+                logger.error("New stack member(s) found!? after upgrade.")
+                logger.error(post_name_diff)
             # if the description diff exists before and after, then "Provisioned" was tacked on or removed
             if pre_desc_diff and post_desc_diff:
                 for d in post_desc_diff:
                     if "Provisioned" in d:
-                        print("CRITICAL {} - OS-mismatch or V-mismatch".format(sw.ipv4_address))
+                        logger.error("Stack member has OS-mismatch or V-mismatch!")
 
-        ###### 5. get VLANs
-        ### TO-DO
-        ##
+        # 5. get VLANs
+        # TO-DO
+        #
         #
 
-        ###### 6. get CDP neighbour state
-
+        # 6. get CDP neighbour state
+        logger.info("Getting CDP neighbours ...")
         sw.post_cdp_neighbour = api_call.get_cdp_neighbours(sw.id)
         sw.post_cdp_neighbour = sorted(sw.post_cdp_neighbour, key=lambda k: k['nearEndInterface'])  # sort the list of dicts
 
         # Using 'nearEndInterface' key. The 'phyInterface' number changes between code upgrade versions
 
         sw.post_cdp_neighbour_nearend = [x['nearEndInterface'] for x in sw.post_cdp_neighbour]  # extract nearEnd values
-        print("{}: CDP NEIGHOURS - {}".format(sw.ipv4_address, sw.post_cdp_neighbour_nearend))
+
+        logger.debug("CDP neighbours: {}".format(sw.post_cdp_neighbour))
+        logger.debug("CDP neighbours near-end: {}".format(sw.post_cdp_neighbour_nearend))
+        logger.info("CDP neighbours stored!")
 
         # compare states
+        logger.info("Comparing CDP neighbour states ...")
         pre_cdp_diff, post_cdp_diff = self.compare_list(sw.pre_cdp_neighbour_nearend, sw.post_cdp_neighbour_nearend)
 
         if not pre_cdp_diff and not post_cdp_diff:
-            print("INFO: {} CDP neighbours are the same before as after".format(sw.ipv4_address))
+            logger.info("CDP neighour(s) are the same pre/post (noice).")
         else:
             # if the name difference exists before but not after ... switch is missing!
             if pre_cdp_diff:
-                print("Neighbour(s) no longer exist! {}".format(pre_cdp_diff))
+                logger.error("CDP neighbour(s) no longer exist!")
+                logger.error(pre_cdp_diff)
             # if the name difference exists after but not before ... switch was found???
             if post_cdp_diff:
-                print("Neighbour(s) detected AFTER code upgrade! {}".format(post_cdp_diff))
+                logger.warning("CDP neighbour(s) found?! after upgrade.")
+                logger.warning(post_cdp_diff)
 
-        print("")
-        print("{}: POST-PROCESSING COMPLETE".format(sw.ipv4_address))
+        logger.info("State comparision and upgrade complete!")
         return True
 
     def push_command(self, *args):
