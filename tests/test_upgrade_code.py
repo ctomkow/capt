67c002cefd75f71d0642a5db8fb1887ec206d770
Craig Tomkow <ctomkow@gmail.com>
2018-07-03 12:12:22 -0600

    added unit tests

--- tests/test_upgrade_code.py
+++ tests/test_upgrade_code.py
@@ -13,14 +13,28 @@ class test_upgrade_code(unittest.TestCase):
     def setUp(self):
 
         self.test_logger = capt.set_logger(self, 'test_logger', logging.DEBUG)
-        self.loopback_ipv4_address = "127.0.0.1"
+        self.loopback = "127.0.0.1"
+        self.blackhole = "0.0.0.0"
+
+        self.test_list1 = ['a', 'b']
+        self.test_list2 = ['b', 'c']
+        self.test_list3 = ['d']
+        self.test_list4 = ['d', 'e']
 
     def test_ping(self):
 
-        self.assertTrue(upgrade_code.ping(self, self.loopback_ipv4_address, self.test_logger))
+        self.assertTrue(upgrade_code.ping(self, self.loopback, self.test_logger))
+        self.assertFalse(upgrade_code.ping(self, self.blackhole, self.test_logger))
 
+    def test_compare_list(self):
 
+        result1, result2 = upgrade_code.compare_list(self, self.test_list1, self.test_list2, self.test_logger)
+        self.assertTrue(result1.pop() == 'a')
+        self.assertTrue(result2.pop() == 'c')
 
+        result1, result2 = upgrade_code.compare_list(self, self.test_list3, self.test_list4, self.test_logger)
+        self.assertFalse(result1) # empty string
+        self.assertTrue(result2.pop() == 'e')
 
 
 if __name__ == '__main__':
