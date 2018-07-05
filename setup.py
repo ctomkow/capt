b0c886d7ae485194675eec5c5ff8a8c8bf3124be
Craig Tomkow <ctomkow@users.noreply.github.com>
2018-07-05 13:49:21 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/ctomkow/capt'
 EMAIL = 'ctomkow@gmail.com'
 AUTHOR = 'Craig Abt Tomkow'
 REQUIRES_PYTHON = '>=3.5.0'
-VERSION = '0.1.4'
+VERSION = '0.1.5'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
