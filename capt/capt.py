f94dc6ddb8c036fb4c2ef1ea8321d3dbe7d88b14
Craig Tomkow <ctomkow@gmail.com>
2018-07-06 13:35:42 -0600

    fix

--- capt/capt.py
+++ capt/capt.py
@@ -69,8 +69,6 @@ class Capt:
         argcomplete.autocomplete(parser)
         args = parser.parse_args()
 
-        print(args)
-
         # handle addressing for ip/mac selection
         if 'ip' in sys.argv or 'mac' in sys.argv:
             addr = craft.normalize_addr(args.address, sys.argv)
